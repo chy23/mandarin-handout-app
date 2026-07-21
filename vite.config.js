4e88b54352f5c8c8efeb4a807e9f11f79a1610a1
chy23 <35660392+chy23@users.noreply.github.com>
2026-07-22 00:39:01 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 確保編譯後的路徑是相對路徑，方便部署到 GitHub Pages
+  base: '/mandarin-handout-app/', // 確保部署到 GitHub Pages 的路徑正確
 })
